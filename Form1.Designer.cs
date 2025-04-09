471635b85fce9b3ea530b8049d7ed7cd7a57fcc0
KhanhLe2710 <leducbaokhanh2710@gmail.com>
2025-04-09 16:19:12 +0700

    US7 - Tạo giao diện popup chọn nền tảng chia sẻ

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,7 @@
             button1 = new Button();
             button2 = new Button();
             checkBox1 = new CheckBox();
+            checkedListBox1 = new CheckedListBox();
             SuspendLayout();
             // 
             // button1
@@ -61,11 +62,20 @@
             checkBox1.Text = "checkBox1";
             checkBox1.UseVisualStyleBackColor = true;
             // 
+            // checkedListBox1
+            // 
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(442, 208);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(150, 114);
+            checkedListBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(checkedListBox1);
             Controls.Add(checkBox1);
             Controls.Add(button2);
             Controls.Add(button1);
@@ -80,5 +90,6 @@
         private Button button1;
         private Button button2;
         private CheckBox checkBox1;
+        private CheckedListBox checkedListBox1;
     }
 }
